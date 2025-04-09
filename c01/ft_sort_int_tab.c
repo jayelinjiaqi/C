1d23c52134b669c362fded6252fc4e0689e0720e
J LIN <116197057+jayelinjiaqi@users.noreply.github.com>
2025-04-10 03:16:58 +0800

    Add files via upload

---
+++ c01/ft_sort_int_tab.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void    ft_sort_int_tab(int *tab, int size)
+{
+    int i;
+    int j;
+    int temp;
+
+    i = 0;
+    while (i < size - 1)
+    {
+        j = 0;
+        while (j < size - 1 - i)
+        {
+            if (tab[j] > tab[j + 1])
+            {
+                temp = tab[j];
+                tab[j] = tab[j + 1];
+                tab[j + 1] = temp;
+            }
+            j++;
+        }
+        i++;
+    }
+}
